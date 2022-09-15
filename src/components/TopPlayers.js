163b5f2fb21e29cb5899bd4c40236ddfd42ac9f8
Zane Fink <zanefink@Zanes-MacBook-Pro.local>
2022-09-15 16:40:28 -0400

    underscore js not working

--- src/components/TopPlayers.js
+++ src/components/TopPlayers.js
@@ -6,25 +6,34 @@ const TopPlayers = (props) => {
     let [topPlayersAPI, setTopPlayersAPI] = useState([])
     let [topSort, setTopSort] = useState([])
 
+
+
 const getTopPlayersAPI = () => {
     axios.get('https://api.sportsdata.io/v3/nfl/stats/json/PlayerGameStatsByWeek/2021REG/7?key=c2d4f67c78294cd4a5ef2cdf2a957a31')
     .then((response) => {
-        setTopSort(response.data.sort((a, b) => {
-            let aFantasyPointsDraftKings = a.updatedAt.toUpperCase();
-            let bFantasyPointsDraftKings = b.updatedAt.toUpperCase();
+        setTopSort = (response)
+        ._sortBy(topSort, 'FantasyPointsDraftKings')
             
-                if (aFantasyPointsDraftKings < bFantasyPointsDraftKings) {
-                    return 1;
-                    }
-                if (aFantasyPointsDraftKings > bFantasyPointsDraftKings) {
-                    return -1;
-                    }
-                    return 0;
-                }));
+
+         //setTopSort(response.data.sort((a, b) => {
+        //     let aFantasyPointsDraftKings = a.updatedAt;
+        //     let bFantasyPointsDraftKings = b.updatedAt;
+            
+        //         if (aFantasyPointsDraftKings < bFantasyPointsDraftKings) {
+        //             return 1;
+        //             }
+        //         if (aFantasyPointsDraftKings > bFantasyPointsDraftKings) {
+        //             return -1;
+        //             }
+        //             return 0;
+        //         }));
       console.log(response.data.slice(0,15))
       setTopPlayersAPI(response.data.slice(0,15))
     })
   }
+
+
+
 
   const handleTopPlayers = (event) => {
     document.getElementById('topPlayers').classList.toggle('showhide');
