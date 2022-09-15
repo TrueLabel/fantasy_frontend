c0488875d1a4fac0c2c5cfc1fb52e2407a927eb8
Zane Fink <zanefink@Zanes-MacBook-Pro.local>
2022-09-15 15:03:03 -0400

    got score included but not sort yet

--- src/components/TopPlayers.js
+++ src/components/TopPlayers.js
@@ -4,37 +4,27 @@ import axios from 'axios'
 
 const TopPlayers = (props) => {
     let [topPlayersAPI, setTopPlayersAPI] = useState([])
-
+    let [topSort, setTopSort] = useState([])
 
 const getTopPlayersAPI = () => {
     axios.get('https://api.sportsdata.io/v3/nfl/stats/json/PlayerGameStatsByWeek/2021REG/7?key=c2d4f67c78294cd4a5ef2cdf2a957a31')
     .then((response) => {
-        
+        setTopSort(response.data.sort((a, b) => {
+            let aFantasyPointsDraftKings = a.updatedAt.toUpperCase();
+            let bFantasyPointsDraftKings = b.updatedAt.toUpperCase();
+            
+                if (aFantasyPointsDraftKings < bFantasyPointsDraftKings) {
+                    return 1;
+                    }
+                if (aFantasyPointsDraftKings > bFantasyPointsDraftKings) {
+                    return -1;
+                    }
+                    return 0;
+                }));
       console.log(response.data.slice(0,15))
       setTopPlayersAPI(response.data.slice(0,15))
     })
   }
-
-
-  
-      setShows(response.data.sort((a, b) => {
-        let aDate = a.updatedAt.toUpperCase();
-        let bDate = b.updatedAt.toUpperCase();
-
-        if (aDate < bDate) {
-          return 1;
-        }
-        if (aDate > bDate) {
-            return -1;
-        }
-        return 0;
-      }));
-    })
-  }
-
-
-
-
 
   const handleTopPlayers = (event) => {
     document.getElementById('topPlayers').classList.toggle('showhide');
@@ -55,9 +45,9 @@ const getTopPlayersAPI = () => {
         <thead>
           <tr>
             <td></td>
-            <td>Element 1</td>
-            <td>Element 2</td>
-            <td>Element 3</td>
+            <td>Name</td>
+            <td>Score</td>
+            <td>Team</td>
           </tr>
         </thead>
         <tbody>
@@ -66,7 +56,7 @@ const getTopPlayersAPI = () => {
             <tr key={player.PlayerID}>
               <td>{index + 1}</td>
               <td>{player.Name}</td>
-              <td>{player.PlayerID}</td>
+              <td>{player.FantasyPointsDraftKings}</td>
               <td>{player.Team}</td>
             </tr>
           )
