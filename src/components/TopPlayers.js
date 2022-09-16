c1a26df2bc04c6589c34871247e69c998f6e8d0c
Zane Fink <zanefink@Zanes-MacBook-Pro.local>
2022-09-16 13:15:55 -0400

    adding select

--- src/components/TopPlayers.js
+++ src/components/TopPlayers.js
@@ -5,11 +5,11 @@ import axios from 'axios'
 const TopPlayers = (props) => {
     let [topPlayersAPI, setTopPlayersAPI] = useState([]);
     let [topPlayers, setTopPlayers] = useState([]);
-    let [year, setYear] = useState(2022);
-    let [week, setWeek] = useState(1);
+    let [year, setYear] = useState(2021);
+    let [week, setWeek] = useState(10);
   
 const getTopPlayersAPI = () => {
-    axios.get('https://api.sportsdata.io/v3/nfl/stats/json/PlayerGameStatsByWeek/2021REG/7?key=c2d4f67c78294cd4a5ef2cdf2a957a31')
+    axios.get('https://api.sportsdata.io/v3/nfl/stats/json/PlayerGameStatsByWeek/' + year + 'REG/' + week + '?key=c2d4f67c78294cd4a5ef2cdf2a957a31')
     .then((response) => {
         let sliceplayers = setTopPlayers(response.data.sort((a, b) => Number(b.FantasyPointsDraftKings) - Number(a.FantasyPointsDraftKings))
         .slice(0, 15));
