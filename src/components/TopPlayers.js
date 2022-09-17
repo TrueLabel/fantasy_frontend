a35e9e96f119466b05859cd5dbb0b2a68765d5af
Zane Fink <zanefink@Zanes-MacBook-Pro.local>
2022-09-17 13:56:25 -0400

    season and week updating

--- src/components/TopPlayers.js
+++ src/components/TopPlayers.js
@@ -8,8 +8,8 @@ const TopPlayers = (props) => {
     let [year, setYear] = useState(2021);
     let [week, setWeek] = useState(10);
   
-const getTopPlayersAPI = () => {
-    axios.get('https://api.sportsdata.io/v3/nfl/stats/json/PlayerGameStatsByWeek/' + year + 'REG/' + week + '?key=c2d4f67c78294cd4a5ef2cdf2a957a31')
+const getTopPlayersAPI = (year, week) => {
+    axios.get('https://api.sportsdata.io/v3/nfl/stats/json/PlayerGameStatsByWeek/' + String(year) + 'REG/' + String(week) + '?key=c2d4f67c78294cd4a5ef2cdf2a957a31')
     .then((response) => {
         let sliceplayers = setTopPlayers(response.data.sort((a, b) => Number(b.FantasyPointsDraftKings) - Number(a.FantasyPointsDraftKings))
         .slice(0, 15));
@@ -23,15 +23,39 @@ const getTopPlayersAPI = () => {
   
 
   useEffect(() => {
-    getTopPlayersAPI()
+    getTopPlayersAPI(year, week)
   }, []);
+
+  const handleChangeYear = (event) => {
+    setYear(event.target.value)
+  }
+
+  const handleChangeWeek = (event) => {
+    setWeek(event.target.value)
+
+  }
+
+
+  const handleSubmitTop = (e) => {
+    e.preventDefault()
+    getTopPlayersAPI(year, week)
+    console.log(year)
+    console.log(week)
+    }
 
   return (
 
     <div className='showhide' id='topPlayers'>
 
       <h4>Top Players</h4>
+      <form>
       <button onClick={handleTopPlayers}>Hide</button>
+      <button onClick={handleSubmitTop}>Submit</button>
+      <label htmlFor="year">Year: </label>
+        <input type="text" name="year" onChange={handleChangeYear} />
+        <label htmlFor="week">Week: </label>
+        <input type="text" name="week" onChange={handleChangeWeek}/>
+        </form>
       <table>
         <thead>
           <tr>
